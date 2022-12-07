9eeefdaf8828ef77e10bf593872cec38802dde65
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2022-12-07 12:46:59 +0100

    gomb generálós metódus

--- js/hangman.js
+++ js/hangman.js
@@ -4,6 +4,21 @@ let mistakes = 0;
 let guessed = [];
 let wordStatus = null;
 
+function generateButtons() {
+    let buttonsHTML = 'a, á, b, c, d, e, é, f, g, h, i, í, j, k, l, m, n, o, ó, ö, ő, p, q, r, s, t, u, ú, ü, ű, v, w, x, y, z'.split(', ').map(letter =>
+      `
+        <button
+          class="btn btn-lg btn-primary m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')"
+        >
+          ` + letter + `
+        </button>
+      `).join('');
+  
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
 function checkIfGameWon() {
     if (wordStatus === answer) {
       document.getElementById('keyboard').innerHTML = 'Nyertél! A megfejtés a kód.';
@@ -31,8 +46,8 @@ function reset() {
     guessed = [];
     document.getElementById('hangmanPic').src = './images/0.jpg';
   
-    guessedWord();
-    updateMistakes();
+    //guessedWord();
+    //updateMistakes();
     generateButtons();
 }
   
